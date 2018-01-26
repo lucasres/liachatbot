e470eb20d7ab6f1277fbc5d840a0e55537a69fec
lucas.res@live.com <lucas.res@live.com>
2018-01-26 14:07:58 -0200

    insert no banco de dados

--- connections.py
+++ connections.py
@@ -8,23 +8,26 @@ class con():
         conn = sqlite3.connect('brain.db')
         cursor = conn.cursor()
 
+        cursor.execute("drop table if exists pattern;")
+        cursor.execute("drop table if exists response;")
         cursor.execute("""
-        drop table if exists pattern;
-        drop table if exists response;
         CREATE TABLE pattern(
-                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                pattern text not NULL,
-                section integer not NULL
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            pattern text not NULL,
+            section integer not NULL
         );
+        """)
+        cursor.execute("""
         CREATE TABLE response(
-                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                response text not NULL,
-                patternId integer not NULL
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            response text not NULL,
+            patternId integer not NULL
         );
         """)
 
+
         print("Brain iniciado com sucesso...")
-        conn.colse()
+        conn.close()
 
     def query(self,sql):
         """
@@ -38,5 +41,35 @@ class con():
         conn.close()
         return True
 
-    def insertPattern(self,pattern):
-        
+    def insertPattern(self,pattern,section):
+        """
+        Insert a new pattern in knowledge
+        :param pattern: String
+        :param section: Integer
+        :return: Boolean
+        """
+        conn = sqlite3.connect('brain.db')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO pattern(pattern,section) VALUES('"+pattern+"',"+str(section)+");")
+        conn.commit()
+        conn.close()
+        return True
+
+    def insertResponse(self,response,patternId):
+        """
+        Insert a new response in knowledge
+        :param pattern: String
+        :param section: Integer
+        :return: Boolean
+        """
+        conn = sqlite3.connect('brain.db')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO response(response,patternId) values('"+response+"',"+str(patternId)+");")
+        conn.commit()
+        conn.close()
+        return True
+
+
+c = con()
+c.insertPattern("bom dia",1)
+c.insertResponse("ola",1)
